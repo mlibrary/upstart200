f3a8741efbb8ef5d229ea8d53107a8484265aa7c
nancymou <nancymou@umich.edu>
2017-05-16 14:49:42 -0400

    Dislay 5 thumbnail images

--- themes/mlibrary/collections/show.php
+++ themes/mlibrary/collections/show.php
@@ -10,17 +10,23 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
 
 <div id="collection-items">
     <p class="view-items-link"><?php echo link_to_items_browse(__('Items in the %s Collection', $collectionTitle), array('collection' => metadata('collection', 'id')));?></p>
+    <?php $counter = 0; ?>
     <?php if (metadata('collection', 'total_items') > 0): ?>
-        <?php foreach (loop('items') as $item): ?>
-        <?php $itemTitle = strip_formatting(metadata('item', array('Dublin Core', 'Title'))); ?>
-        <div class="item hentry">
-            <?php if (metadata('item', 'has thumbnail')): ?>
-            <div class="item-img">
-                <?php echo link_to_item(item_image('square_thumbnail', array('alt' => $itemTitle))); ?>
-            </div>
-            <?php endif; ?>
-        </div>
-        <?php endforeach; ?>
+      <?php foreach (loop('items') as $item): ?>
+        <?php if ($counter > 5): ?>
+           <?php break;?>
+        <?php else: ?>
+           <?php $itemTitle = strip_formatting(metadata('item', array('Dublin Core', 'Title'))); ?>
+             <div class="item hentry">
+              <?php if (metadata('item', 'has thumbnail')): ?>
+                <div class="item-img">
+                  <?php echo link_to_item(item_image('square_thumbnail', array('alt' => $itemTitle))); ?>
+                </div>
+              <?php endif; ?>
+             </div>
+        <?php endif; ?>
+           <?php $counter++;?>
+      <?php endforeach; ?>
     <?php else: ?>
         <p><?php echo __("There are currently no items within this collection."); ?></p>
     <?php endif; ?>
