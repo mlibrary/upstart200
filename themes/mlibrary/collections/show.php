0ad254c98fd208a3147ba3f94d66bffc616457d3
nancymou <nancymou@umich.edu>
2017-05-16 15:12:38 -0400

    Theme the show page to display 5 images

--- themes/mlibrary/collections/show.php
+++ themes/mlibrary/collections/show.php
@@ -10,7 +10,7 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
 
 <div id="collection-items">
     <p class="view-items-link"><?php echo link_to_items_browse(__('Items in the %s Collection', $collectionTitle), array('collection' => metadata('collection', 'id')));?></p>
-    <?php $counter = 0; ?>
+    <?php $counter = 1; ?>
     <?php if (metadata('collection', 'total_items') > 0): ?>
       <?php foreach (loop('items') as $item): ?>
         <?php if ($counter > 5): ?>
@@ -20,7 +20,7 @@ $collectionTitle = strip_formatting(metadata('collection', array('Dublin Core', 
              <div class="item hentry">
               <?php if (metadata('item', 'has thumbnail')): ?>
                 <div class="item-img">
-                  <?php echo link_to_item(item_image('square_thumbnail', array('alt' => $itemTitle))); ?>
+                  <?php echo link_to_item(item_image('square_thumbnail', array('height'=>"42", 'width'=>"42", 'alt' => $itemTitle))); ?>
                 </div>
               <?php endif; ?>
              </div>
